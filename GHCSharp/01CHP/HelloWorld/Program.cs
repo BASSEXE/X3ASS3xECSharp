40a8132e8fc58b93c8b37e10fcbd9ebaf84e3095
X3ASS3xE <45321052+BASSEXE@users.noreply.github.com>
2020-02-03 23:57:41 -0600

    Added Chapter 1 Files

---
+++ GHCSharp/01CHP/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿//Author: X3ASSEXE
+// Title:HelloWorld.cs
+//Summary: Simple HelloWorld Program
+using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string HWorld = "Hello World!";
+            DateTime DNow = DateTime.Now;
+            Console.Write(HWorld);
+            Console.WriteLine("The date is " + DNow.ToLongDateString());
+        }
+    }
+}
