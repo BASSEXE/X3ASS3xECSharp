a678c107eda9683fa309507b4919e91bed224aeb
X3ASS3xE <45321052+BASSEXE@users.noreply.github.com>
2020-02-17 21:12:23 -0600

    Summary

---
+++ GHCSharp/01CHP/pinvoke/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Pinvoke
+{
+    class Program
+    {
+      [DllImport("user32", CharSet=CharSet.Auto)]
+      static extern int MessageBox(IntPtr hWnd,
+              String text, String Caption, int options);
+      [DllImport("libc")]
+      static extern void printf(string message);
+
+
+        static void Main(string[] args)
+        {
+          OperatingSystem os = Environment.OSVersion;
+
+          if(os.Platform == PlatformID.Win32Windows ||  os.Platform == PlatformID.Win32NT)
+          {
+            MessageBox (IntPtr.Zero,  "Hello World!", "Hello World!", 0);
+          }
+          else
+          {
+            printf ("Hello World!");
+          }
+        }
+    }
+}
