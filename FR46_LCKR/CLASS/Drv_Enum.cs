495090cd62d8a1d4227db59e288f9aa1f66403c3
reyreyiam <elyziahreyes@gmail.com>
2020-10-06 14:59:34 -0500

    Drive Enumeration Funciton
    Drive Enumeration Funciton

--- FR46_LCKR/CLASS/Drv_Enum.cs
+++ FR46_LCKR/CLASS/Drv_Enum.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace FR46_LCKR
+namespace FR46_LCKR.CLASS
 {
-    internal class Drv_Enum
+    internal class Foldr_Browser : IDrv_Enum
     {
-        public Drv_Enum()
+        public List<string> StartFolder()
         {
+            var cDrvs = DriveInfo.GetDrives().Select(drive => drive.Name).ToList();
+            cDrvs =.AddRange(GetSpcl_Folders());
+            return cDrvs;
+        }
+
+        private List<string> GetSpcl_Folders()
+        {
+
         }
     }
 }
